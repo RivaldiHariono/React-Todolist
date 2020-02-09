64c64f8dff79d3b900b6a9fcca73182bf4cf1cf1
RivaldiHariono <40395498+RivaldiHariono@users.noreply.github.com>
2020-02-09 17:51:20 +0700

    split to components

--- src/App.js
+++ src/App.js
@@ -2,34 +2,16 @@ import React from "react";
 import "./styles.css";
 
 import Paper from "./components/paper";
+import Header from "./components/header";
+import Todoform from "./components/todoform";
+import Todos from "./components/todos";
 export default function App() {
   return (
     // className nama lain dari class
     <Paper>
-      <section className="header">
-        <button className="header-btn">Add</button>
-        <h1 className="header-title">Todo List</h1>
-        <button className="header-btn main-red-color">Clear</button>
-      </section>
-
-      <section className="add">
-        <form className="add-form">
-          <input type="text" className="add-input" />
-          <button className="add-btn">Add</button>
-        </form>
-      </section>
-
-      <section className="todos">
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-        <div className="todo">
-          <span className="todo-text">Learning React</span>
-        </div>
-      </section>
+      <Header />
+      <Todoform />
+      <Todos />
     </Paper>
   );
 }
---
+++ src/components/todoform.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const todoform = () => {
+  return (
+    <section className="add">
+      <form className="add-form">
+        <input type="text" className="add-input" />
+        <button className="add-btn">Add</button>
+      </form>
+    </section>
+  );
+};
+
+export default todoform;
