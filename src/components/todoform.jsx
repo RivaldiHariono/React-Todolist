fae87f9f88e2fe1df04b8d6273cc7a697dbfa639
RivaldiHariono <40395498+RivaldiHariono@users.noreply.github.com>
2020-02-13 20:15:39 +0700

    membuat todoform

--- src/components/todoform.jsx
+++ src/components/todoform.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+// useState = mini database di dalam todoform ini
+const todoform = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    if (value === "") {
+      alert("No ToDo");
+    } else {
+      addTodo(value);
+      alert(value);
+      setValue("");
+    }
+  };
 
-const todoform = () => {
+  console.log("value", value);
   return (
     <section className="add">
-      <form className="add-form">
-        <input type="text" className="add-input" />
+      <form className="add-form" onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          className="add-input"
+          onChange={e => setValue(e.target.value)}
+          value={value}
+        />
         <button className="add-btn">Add</button>
       </form>
     </section>
@@ -12,3 +31,6 @@ const todoform = () => {
 };
 
 export default todoform;
+todoform.propTypes = {
+  addTodo: PropTypes.func.isRequired
+};
